0e475f53fce7d936e7c54ff15f3001a689bb65a4
Marcin <marcino451@gmail.com>
2018-03-17 22:50:39 +0100

    created project , randomizing some cells

---
+++ Sudoku_gui/Sudoku_gui/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Sudoku_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(490, 326);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.Zmienione);
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 10F;
+            this.Column1.HeaderText = "";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            this.Column2.FillWeight = 10F;
+            this.Column2.HeaderText = "";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 40;
+            // 
+            // Column3
+            // 
+            this.Column3.FillWeight = 10F;
+            this.Column3.HeaderText = "";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 40;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 40;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 40;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 40;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 40;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 40;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 40;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 351);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+    }
+}
+
---
+++ Sudoku_gui/Sudoku_gui/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ColumnHeadersVisible = false;
+            int[,] tab = new int[9, 9];
+            tab = LosujPlansze();
+            dataGridView1 = DodajLosowaneLiczby(tab, dataGridView1);
+           // Sprawdz(dataGridView1);
+        }
+
+        public static void Sprawdz(DataGridView dataGrid)
+        {
+           int size = dataGrid.Rows.Count;
+        }
+
+        public static DataGridView DodajLosowaneLiczby(int[,] tab, DataGridView dataGrid)
+        {
+            
+            for (int i=0; i<9; i++) // wiersze
+            {
+                string[] row = new string[9];
+                for (int j=0; j<9; j++) // kolumny
+                {
+                    if( tab[i, j] > 0)
+                    {
+                        row[j] = tab[i, j].ToString();
+                    }
+                }
+                dataGrid.Rows.Add(row);
+            }
+            return dataGrid;
+        }
+
+        
+
+        public static int[,] LosujPlansze()
+        {
+            int[,] tab = new int[9, 9];
+            Random random = new Random();
+            // uzupelniamy tablice z liczbami losowymi cyframi 1-9
+            for (int i = 0; i < 9; i++)
+            {
+                int x = random.Next(0, 8);
+                int y = random.Next(0, 8);
+
+                tab[x, y] = random.Next(1, 9);
+                //if (SprawdzPlanszeR() > 0)
+                //{
+                //    int aktualna = tab[x, y];
+                //    tab[x, y] = random.Next(1, 9);
+                //    if (aktualna == tab[x, y])
+                //    {
+                //        tab[x, y] = random.Next(1, 9);
+                //    }
+                //}
+            }
+            return tab;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Zmienione(object sender, DataGridViewCellEventArgs e)
+        {
+            int x = e.RowIndex;
+            int y = e.ColumnIndex;
+
+            MessageBox.Show("Zmieniono cos !");
+        }
+    }
+}
+
+/*
+ * 
+ * class Program
+    {
+        static int[,] tab = new int[9, 9]; // Tabelka 9x9
+
+        public static void RysujPlansze()
+        { // rysujemy plansze 
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (tab[i, j] == 0)
+                    {
+                        Console.Write(" - "); // jesli jakis element jest zerem - to rysujemy kreske 
+                    }
+                    else
+                    {
+                        Console.Write(" " + tab[i, j] + " "); // jezeli nie, to wypisujemy ten element 
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public static void LosujPlansze()
+        {
+            // w przyszlosci zmienic na zwracanie tablicy ewentualnie zwracanie liczb ktore zostały wylosowane tak, by nie mozna bylo zmienic ich podczas wypelniania sudoku
+            Random random = new Random();
+            // uzupelniamy tablice z liczbami losowymi cyframi 1-9
+            for (int i=0; i<9; i++)
+            {
+                int x = random.Next(0, 8);
+                int y = random.Next(0, 8);
+
+                tab[x, y] = random.Next(1, 9);
+                if (SprawdzPlanszeR() > 0)
+                {
+                    int aktualna = tab[x, y];
+                    tab[x, y] = random.Next(1, 9);
+                    if (aktualna == tab[x, y])
+                    {
+                        tab[x, y] = random.Next(1, 9);
+                    }
+                }
+            }
+        }
+
+        public static void ZmienCos()
+        { // zmienanie elementu
+            Console.WriteLine("x : ");
+            int x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("x : ");
+            int y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Wartosc : ");
+            tab[x, y] = Int32.Parse(Console.ReadLine());
+            RysujPlansze();
+        }
+
+        public static int checkNulls()
+        { // sprawdz ile jeszcze zer pozostało do wypelnienia
+            int nulls = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (tab[i, j] == 0)
+                    {
+                        nulls++;
+                    }
+                }
+            }
+            return nulls;
+        }
+
+        public static int checkCell(int wiersz, int kolumna)
+        { // sprawdzanie komorki
+            int temp = tab[wiersz, kolumna];
+            int err = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (tab[i, kolumna] == 0) continue;
+                if (wiersz == i) continue;
+                if (temp == tab[i, kolumna])
+                {
+                    err++;
+                }
+            }
+
+            for (int j = 0; j < 9; j++)
+            {
+                if (tab[wiersz, j] == 0) continue;
+                if (kolumna == j) continue;
+                if (temp == tab[wiersz, j])
+                {
+                    err++;
+                }
+            }
+            return err;
+        }
+
+        public static void SprawdzPlansze()
+        { // sprawdzanie planszy w poszukiwaniu bledow
+            int errors = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int err = checkCell(i, j);
+                    errors = errors + err;
+                }
+            }
+            if (errors > 0)
+            {
+                Console.WriteLine("Sa bledy");
+            }
+            else
+            {
+                Console.WriteLine("Nie ma bledow");
+            }
+            int nulls = checkNulls(); // pobieramy ilosc zer
+            if (nulls == 0 && errors == 0)
+            { // jezeli nie ma juz pustych pol i nie ma bledow to rozwiazane sudoku
+                Console.WriteLine("GRATULACJE !!!! ROZWIĄZAŁEŚ SUDOKU.");
+            }
+        }
+
+        public static int SprawdzPlanszeR()
+        {
+            int errors = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int err = checkCell(i, j);
+                    errors = errors + err;
+                }
+            }
+            if (errors > 0)
+            {
+                return errors;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            int w = 0;  // zmienna sterujaca, na razie bezuzyteczna
+            int m = 0; // ruchy
+            LosujPlansze();
+            RysujPlansze();
+            ZmienCos();
+            m++;
+            while (w == 0)
+            {
+                ZmienCos();
+                m++;
+                if (m > 2)
+                {
+                    SprawdzPlansze();
+                }
+
+            }
+        }
+    }
+
+*/
